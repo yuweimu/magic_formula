6102e3c09884e413770c04d99e745b7394c9db4a
lz <lz@10-10-64-105.(none)>
2016-02-22 17:35:50 +0800

    sql syntax err fix

--- fix_f10.sh
+++ fix_f10.sh
@@ -6,8 +6,9 @@ scale='1.5'
 cat fix_f10_2015.conf | while read line; do
 	  code=$(echo $line | awk '{print $1}')
 	  scale=$(echo $line | awk '{print $2}')
-    sql="UPDATE fiscal_year_2014 SET earning_per_share=earning_per_share/$scale,book_value_per_share=book_value_per_share/2.0,fixed=1 WHERE stock_code=\'$code\' AND fixed=0;"
+    sql="UPDATE fiscal_year_2014 SET earning_per_share=earning_per_share/$scale,book_value_per_share=book_value_per_share/2.0,fixed=1 WHERE stock_code='$code' AND fixed=0;"
     echo $sql 
+    echo $sql | mysql -uroot magic_formula
 done
 
 
